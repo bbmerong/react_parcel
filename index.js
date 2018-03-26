d8394ec7f6379b9cbfc8858541779dfd1af890ec
bbmerong <bbmerong@naver.com>
2018-03-27 00:24:20 +0900

    add App.js

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
 
-const App = ()=>{
-    return(
-        <div>
-            Hello React;
-        </div>
-    );
-}
 
 ReactDOM.render(<App/>,document.getElementById("app"));
